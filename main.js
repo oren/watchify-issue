1a782cd70aa3155d96f9dcdb598d47bb182e0483
Oren <ogolan@yp.com>
2014-11-03 18:18:29 -0800

    init

---
+++ main.js
@@ -0,0 +1,20 @@
+document.addEventListener('load', function() {
+  var $$ = document.querySelectorAll.bind(document);
+
+  unwrapImages();
+
+  function unwrapImages () {
+    var divs = $$('#defered-images img[data-src]');
+    // borrowing forEach from Array since divs is not an array
+    [].forEach.call(divs, unwrap);
+  }
+
+  function unwrap (img) {
+    console.log('img', img);
+    img.src = img.getAttribute('data-src');
+    img.removeAttribute('data-src');
+  }
+
+  console.log('foo');
+});
+
---
+++ readme.md
@@ -0,0 +1,4 @@
+# watchify not working for me!
+
+    npm install
+    ./node_modules/.bin/watchify main.js -o bundle.js -v
